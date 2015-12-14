b562fb5018f1f92961a9f75f725a2e91736ca82b
Arkadiusz <arkmon@me.com>
2015-12-14 08:20:32 +0000

    Initial Commit

---
+++ Nebuchadnezzar/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Nebuchadnezzar
+//
+//  Created by Arkadiusz on 14/12/2015.
+//  Copyright © 2015 Arkadiusz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Nebuchadnezzar/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Nebuchadnezzar
+//
+//  Created by Arkadiusz on 14/12/2015.
+//  Copyright © 2015 Arkadiusz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
